f0553a0eb0c7c2c91c599775664c685f529fdf53
Quentin Astegiano <quentin.astegiano@gmail.com>
2016-04-25 00:56:55 +0200

    Squellette du site

--- src/main/java/com/astegiano/sante/Server.java
+++ src/main/java/com/astegiano/sante/Server.java
@@ -1,6 +1,10 @@
 package com.astegiano.sante;
 
+import ratpack.guice.Guice;
+import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
+import ratpack.thymeleaf.Template;
+import ratpack.thymeleaf.ThymeleafModule;
 
 /**
  * Created by Quentin on 24/04/2016.
@@ -8,11 +12,21 @@ import ratpack.server.RatpackServer;
 public class Server {
 
     public static void main(String... args) throws Exception {
+
+        ThymeleafModule thymeleafModule = new ThymeleafModule();
+        thymeleafModule.setTemplatesPrefix("templates");
+
         RatpackServer.start(server -> server
+                .serverConfig(b -> b.baseDir(BaseDir.find("webapp/.ratpack")))
+                .registry(Guice.registry(b -> b.module(ServerModule.class)))
+                .registry(Guice.registry(b -> b.module(thymeleafModule)))
                 .handlers(chain -> chain
-                        .get(ctx -> ctx.render("Hello World!"))
-                        .get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!"))
+                        .files(b -> b.dir("assets"))
+                        .get(ctx -> ctx.render(Template.thymeleafTemplate("home")))
+                        .get("videos", ctx -> ctx.render(Template.thymeleafTemplate("videos")))
                 )
         );
     }
+
+
 }
---
+++ src/main/java/com/astegiano/sante/ServerModule.java
@@ -0,0 +1,14 @@
+package com.astegiano.sante;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * Created by Quentin on 24/04/2016.
+ */
+public class ServerModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+}
